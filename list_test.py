d85dfdc573caf1136e031bb702fd14e6446d92ce
Reethick Rosan <143926519+whoisreethick@users.noreply.github.com>
2026-03-25 21:17:16 +0530

    list assignment

---
+++ list_test.py
@@ -0,0 +1,41 @@
+# 🎯 Your task: Build a simple music player (using lists)
+
+# Start very small. Don’t overcomplicate.
+
+# 🔹 Core features (Phase 1)
+
+# You must implement:
+
+# Add a song
+# Show playlist
+# Play next song (remove from front)
+
+def create_playlist():
+    playlist = []
+    return playlist
+
+def enter_songs(playlist):
+    while True:
+        song = input("Enter the songs you want to listen to. Type 'stop' to stop adding songs to the queue:")
+        if song.strip() == "":
+            playlist.pop()
+        if song == "stop":
+            break
+        playlist.append(song)
+    if not playlist:
+        print("Playlist cannot be empty. Enter atleast one song.")
+        enter_songs(playlist)
+    return playlist
+
+def play_song(playlist):
+    print(f"Now playing: {playlist.pop(0)}")
+    
+def show_playlist(playlist):
+    for song in playlist:
+        print(song)
+
+# def skip_song(playlist):
+#     for song in playlist:
+#         if song:
+#             continue
+#     print(f"Song skipped. Now playing{song}")
